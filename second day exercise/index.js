1d104d18c97ccc5d3a684f2686abd6dfaaaa29a0
sandeshgiri98 <sandeshgiri022@gmail.com>
2024-06-25 21:08:53 +0545

    Second day

---
+++ second day exercise/index.js
@@ -0,0 +1,14 @@
+function myFunction(){
+  alert('Add to bag is clicked');
+}
+
+function OpFunction(){
+  alert('Wishlist is clicked');
+}
+
+function Jptfunction(){
+  alert("Added to Cart Successfully");
+}
+function Optfunction(){
+  alert("Your wishlisht is here")
+}
