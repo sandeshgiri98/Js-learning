e379efa9be31854327936db748170224959e6359
sandeshgiri98 <sandeshgiri022@gmail.com>
2024-06-30 19:52:31 +0545

    Programme

---
+++ brocode/index.js
@@ -0,0 +1,9 @@
+let age = 32;
+let price = 457.75;
+let gpa = 3.30;
+
+
+console.log(typeof(age));
+console.log(`Your age is ${age} years old.`);
+console.log(`Your price is ${price}`);
+console.log(`Your GPA is ${gpa*25}, it shouds good`);
---
+++ brocode/vairables/index.js
@@ -0,0 +1,33 @@
+// variable: a container that stores value
+
+// let x;
+// x= 100;
+// let age= 25; 
+// let  price = 23.54;
+// let gpa= 3.4;
+// let firstname = 'Apple';
+// let lastname = 'Ball';
+
+
+
+// console.log(x);
+// console.log(`your age is ${age} years old`);
+// console.log(`The price of an apple is ${price}`);
+// console.log(`The gpa of mine is ${gpa}`);
+// console.log(typeof(age));
+// console.log(typeof(price));
+// console.log(typeof(firstname));
+// console.log(typeof(lastname));
+// console.log(`my name is `+firstname+ ' ' +lastname);
+
+
+// let forSale = true;
+// let isStudent = true;
+
+// console.log(`Is the car for sale? :${forSale}`);
+
+
+let fullName = "Bro Code";
+let age = 25;
+let Student = false;
+
